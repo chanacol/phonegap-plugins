b90d35020becd1f24f38930c61da1c7d37ca46ba
Andrew Glover <ajglover@gmail.com>
2011-09-23 10:19:02 -0400

    initial commit of App47 PhoneGap plugin for iOS

---
+++ ios/app47pg.js
@@ -0,0 +1,47 @@
+if (!PhoneGap.hasResource("App47")) {
+	PhoneGap.addResource("App47");
+
+    App47 = function() {
+        this.serviceName = "com.app47.sdk";
+    };         
+    
+    App47.prototype.sendGenericEvent = function(message, success, fail) {
+        return PhoneGap.exec(success, fail, this.serviceName, "sendGenericEvent", message);
+    };
+    
+    App47.prototype.startTimedEvent = function(name, success, fail) {
+        return PhoneGap.exec(success, fail, this.serviceName, "startTimedEvent", name);
+    };
+    
+    App47.prototype.endTimedEvent = function(name, success, fail) {
+        return PhoneGap.exec(success, fail, this.serviceName, "endTimedEvent", name);
+    };
+    
+    App47.prototype.debug = function(message, success, fail) {
+        return PhoneGap.exec(success, fail, this.serviceName, "log", [{type:"debug", msg:message}]);
+    };
+    
+    App47.prototype.info = function(message, success, fail) {
+        return PhoneGap.exec(success, fail, this.serviceName, "log", [{type:"info", msg:message}]);
+    };
+    
+    App47.prototype.warn = function(message, success, fail) {
+        return PhoneGap.exec(success, fail, this.serviceName, "log", [{type:"warn", msg:message}]);
+    };
+    
+    App47.prototype.error = function(message, success, fail) {
+        return PhoneGap.exec(success, fail, this.serviceName, "log", [{type:"error", msg:message}]);
+    };
+    
+    App47.prototype.getValue = function(grp, ky, success, fail) {
+        return PhoneGap.exec(success, fail, this.serviceName, "configurationValue", [{group:grp, key:ky}]);
+    };
+    
+    App47.install = function(){
+        if (typeof window.plugins == "undefined") window.plugins = {};
+        if (typeof window.plugins.app47 == "undefined") window.plugins.app47 = new App47();
+        return window.plugins.app47;
+    };
+    
+    PhoneGap.addConstructor(App47.install);
+} 
